02d8526c27e373fea589e1800c9d52895c6a4cf5
dkkc <kowal_1977@wp.pl>
2018-04-18 21:14:32 +0200

    clearing

--- src/App.js
+++ src/App.js
@@ -50,22 +50,6 @@ class App extends Component {
         }   
     }
 
-    getNameDay = (args) => {
-        let  date = new Date(args);
-         const days = [];
-
-         days[1] = 'Poniedziałek';
-         days[2] = 'Wtorek';
-         days[3] = 'Środa';
-         days[4] = 'Czwartek';
-         days[5] = 'Piątek';
-         days[6] = 'Sobota';
-         days[0] = 'Niedziela';
-         
-         let dayOfWeek = days[date.getDay()];
-        
-         return dayOfWeek;
-     }
     render() {
         
        let city = this.state.searchCity;
